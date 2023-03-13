964e854d8603ef65c0106ca519e4b759cffeabaa
EmmanuelOdedele <emmaodedele@gmail.com>
2023-03-13 16:59:47 +0000

    commit

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #444;
+  color: white;
   margin-top: 60px;
 }
 </style>
